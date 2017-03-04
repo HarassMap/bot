6c5aa209a90275cf7a40ccf788b0f63728cb8dd9
Mohamed Shaaban <mohamed.shaaban@zalando.de>
2017-03-04 15:38:25 +0100

    hello world

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var app = express();
+'use strict'
 
-app.set('port', (process.env.PORT || 5000));
+const VERIFY_TOKEN = 'catch-me-if-you-can';
 
-app.use(express.static(__dirname + '/public'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+    console.log('running on port', app.get('port'))
+})
