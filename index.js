c4097f5bc4841729e04941ff749e063b1355ba0f
Mohamed Shaaban <mohamed.shaaban@zalando.de>
2017-05-31 00:33:26 +0200

    Add static files. Add one-time scripts

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 const VERIFY_TOKEN = process.env.FB_VERIFFY_TOKEN;
 const token = process.env.FB_PAGE_ACCESS_TOKEN;
-const PAGE_ID = '1365749950157719';
+const PAGE_ID = '134095127145328';
 
+const OneTimeSetup = require('./one-time-scripts');
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -16,14 +17,20 @@ const components = require('./src/components/');
 const { ScenarioFactory } = require('./src/components/scenario');
 const Request = require('./src/utils/request');
 
+app.use('/', express.static(__dirname + '/public'));
+
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(UsersPlugin);
 app.use(TranslationsPlugin);
 
-app.get('/', function (req, res) {
+app.get('/whoami', function (req, res) {
     res.send('Hello world, I am a chat bot')
+});
+
+app.get('/details', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/details.html'));
 });
 
 // for Facebook verification
@@ -138,11 +145,22 @@ app.post('/webhook/', function (request, response) {
             reply(request);
         } else if (event.postback && event.postback.payload) {
             console.log('Received postback: %s', event.postback.payload);
-            handlePostBack(request, event);
+
+            if (event.postback.payload === 'SHOW_DETAILS') {
+
+            } else {
+                handlePostBack(request, event);
+            }
         }
     }
     response.sendStatus(200);
 });
+
+
+OneTimeSetup.setGreetingText();
+OneTimeSetup.setDomainWhitelist();
+OneTimeSetup.setGetStarted();
+OneTimeSetup.setPersistentMenu();
 
 // Spin up the server
 app.listen(app.get('port'), function() {
--- one-time-scripts.js
+++ one-time-scripts.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const token = process.env.FB_PAGE_ACCESS_TOKEN;
+const SERVER_URL = process.env.SERVER_URL;
 
 function dispatchRequest(url, data) {
     return request({
@@ -46,4 +47,75 @@ function setGreetingText() {
     return dispatchRequest('https://graph.facebook.com/v2.6/me/messenger_profile', data);
 }
 
-setGetStarted();
+function setDomainWhitelist() {
+    const data = {
+        'whitelisted_domains':[
+            'https://petersfancyapparel.com'
+        ]
+    };
+
+    return dispatchRequest('https://graph.facebook.com/v2.6/me/messenger_profile', data);
+}
+
+function setPersistentMenu() {
+    const englishCTA = [
+        {
+            "type": "postback",
+            "title": "Help",
+            "payload": "GET_STARTED"
+        },
+        {
+            "type": "postback",
+            "title": "Report Incident",
+            "payload": "REPORT_INCIDENT"
+        },
+        {
+            type: 'web_url',
+            title: 'Show Details',
+            url: `${SERVER_URL}/details`,
+            webview_height_ratio: 'tall',
+            messenger_extensions: true
+        }
+    ];
+
+    const data = {
+        "persistent_menu":[
+
+            {
+                "locale": "default",
+                "composer_input_disabled": false,
+                "call_to_actions": englishCTA
+            },
+            {
+                "locale": "en_US",
+                "composer_input_disabled": false,
+                "call_to_actions": englishCTA
+            },
+            {
+                "locale": "ar_AR",
+                "composer_input_disabled": false,
+                "call_to_actions": [
+                    {
+                        "type": "postback",
+                        "title": "المساعدة",
+                        "payload": "GET_STARTED"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "الإبلاغ عن حالة تحرش",
+                        "payload": "REPORT_INCIDENT"
+                    }
+                ]
+            }
+        ]
+    };
+
+    return dispatchRequest('https://graph.facebook.com/v2.6/me/messenger_profile', data);
+}
+
+module.exports = {
+    setGreetingText,
+    setDomainWhitelist,
+    setGetStarted,
+    setPersistentMenu
+};
